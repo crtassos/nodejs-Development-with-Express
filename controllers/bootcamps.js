f7e347dad38d2de9dd7b1f4dcd4322d42105a648
Tassos Vassilopoulos <nvrsr@hotmail.com>
2021-01-31 19:03:35 +0000

    1) error handling 2) async/await with express

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -1,34 +1,76 @@
+const Bootcamp = require('../models/Bootcamp')
+const ErrorResponse = require('../utils/errorResponse.js')
+const asyncHandler = require('../middleware/async')
+
 //@desc   GET all bootcamps
 //@route  GET /api/v1/bootcamps
 //@access Public
-exports.getBootcamps = (req,res,next)=>{
-    res.status(200).json({success: true, msg: 'Show all bootcamps'})
-}
+exports.getBootcamps = asyncHandler(async (req,res,next)=>{
+
+        const bootcamps = await Bootcamp.find()
+
+        res.status(200).json({ 
+            success:true,
+            count:bootcamps.length,
+            data:bootcamps })
+})
 
 //@desc   GET single bootcamp
 //@route  GET /api/v1/bootcamps/:id
 //@access Public
-exports.getBootcamp = (req,res,next)=>{
-    res.status(200).json({success: true, msg: `Get bootcamp ${req.params.id}`})
-}
+exports.getBootcamp = asyncHandler(async (req,res,next)=>{
+
+    const bootcamp = await Bootcamp.findById(req.params.id)
+    res.status(200).json({ success:true, data:bootcamp })
+
+    if(!bootcamp){
+        return next(new ErrorResponse(`BootCamp not found with id ${req.params.id}`,404))
+    }
+})
 
 //@desc   CREATE bootcamp
 //@route  POST /api/v1/bootcamps
 //@access Private
-exports.createBootcamp = (req,res,next)=>{
-    res.status(200).json({success: true, msg: 'Create new bootcamp'})
-}
+exports.createBootcamp = asyncHandler(async (req,res,next)=>{
+
+        const bootcamp = await Bootcamp.create(req.body)
+        res.status(201).json({
+        success:true,
+        data: bootcamp
+    })
+})
 
 //@desc   Update bootcamp
 //@route  PUT /api/v1/bootcamps/:id
 //@access Private
-exports.updateBootcamp = (req,res,next)=>{
-    res.status(200).json({success: true, msg: `Update bootcamp ${req.params.id}`})
-}
+exports.updateBootcamp = asyncHandler(async (req,res,next)=>{
+
+    const bootcamp = 
+        await Bootcamp.findByIdAndUpdate(req.params.id, req.body,
+            {
+                new: true,
+                runValidators:true
+            })
+
+    if(!bootcamp){
+        return next(new ErrorResponse(`BootCamp not found with id ${req.params.id}`,404))
+    }
+
+        res.status(200).json({ success:true,data: bootcamp })
+})
 
 //@desc   Delete bootcamp
 //@route  Delete /api/v1/bootcamps/:id
 //@access Private
-exports.deleteBootcamp = (req,res,next)=>{
-    res.status(200).json({success: true, msg: `delete bootcamp ${req.params.id}`})
-}
+exports.deleteBootcamp = asyncHandler(async (req,res,next)=>{
+
+    const bootcamp = 
+        await Bootcamp.findByIdAndDelete(req.params.id)
+
+    if(!bootcamp){
+        return next(new ErrorResponse(`BootCamp not found with id ${req.params.id}`,404))
+    }
+
+        res.status(200).json({ success:true,data: {} })
+
+})
--- server.js
+++ server.js
@@ -3,7 +3,9 @@ const dotenv  = require('dotenv')
 //Route files
 const bootcamp = require('./routes/bootcamps')
 const morgan = require('morgan')
+const colors = require('colors')
 const connectDB = require('./config/db')
+const errorHandler = require('./middleware/error')
 
 //Load env vars
 dotenv.config({path: './config/config.env'})
@@ -13,6 +15,9 @@ connectDB()
 
 const app = express()
 
+//Body parser
+app.use(express.json())
+
 // dev logging middleware
 if(process.env.NODE_ENV === 'development'){
     app.use(morgan('dev'))
@@ -21,15 +26,19 @@ if(process.env.NODE_ENV === 'development'){
 //Mount the routers
 app.use('/api/v1/bootcamps', bootcamp)
 
+//Register the custom error handler
+app.use(errorHandler)
+
 const PORT = process.env.PORT || 5000
 
 const server = app.listen(
     PORT,
-    console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`))
+    console.log(`Server running in 
+        ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold))
 
 // Handle unhandled promise rejections
 process.on('unhandledRejection',(err,promise)=>{
-    console.log(`Error: ${err.message}`)
+    console.log(`Error: ${err.message}`.red)
     //Close server and exit process
     server.close(()=>process.exit(1))
 })
