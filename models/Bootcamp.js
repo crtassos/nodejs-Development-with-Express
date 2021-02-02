49a793527e40e3dd2cdfc10746c3f704236b8f9e
Tassos Vassilopoulos <nvrsr@hotmail.com>
2021-02-02 15:07:01 +0000

    1) Added mongodb middleware functions in schema file 2) added GEOcoder package. Formate location data in schema

--- models/Bootcamp.js
+++ models/Bootcamp.js
@@ -1,4 +1,7 @@
 const mongoose = require('mongoose')
+const slugify = require('slugify')
+const geocoder = require('../utils/geocoder')
+
 
 const BootcampSchema = new mongoose.Schema({
     name:{
@@ -41,11 +44,9 @@ const BootcampSchema = new mongoose.Schema({
         type: {
             type: String, // Don't do `{ location: { type: String } }`
             enum: ['Point'], // 'location.type' must be 'Point'
-            required: false
           },
           coordinates: {
             type: [Number],
-            required: false,
             index:'2dsphere'
           },
           formattedAddress: String,
@@ -98,6 +99,31 @@ const BootcampSchema = new mongoose.Schema({
       type: Date,
       default: Date.now
     },
+})
+
+BootcampSchema.pre('save', function(next){
+  this.slug = slugify(this.name, { lower: true })
+  next()
+})
+
+// Geocode & create location field
+BootcampSchema.pre('save', async function(next){
+
+  const loc = await geocoder.geocode(this.address)
+  this.location = {
+    type: 'Point',
+    coordinates:[loc[0].longitude,loc[0].latitude],
+    formattedAddress: loc[0].formattedAddress,
+    street: loc[0].streetName,
+    city: loc[0].city,
+    state: loc[0].stateCode,
+    zipcode: loc[0].zipcode,
+    country: loc[0].country,
+  }
+
+  //Do not save address in DB
+  this.address = undefined;
+  next();
 })
 
 module.exports = mongoose.model('Bootcamp', BootcampSchema)
