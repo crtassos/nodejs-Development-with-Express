f050913dad9120f64c2c0adde27fe982f39ed709
Tassos Vassilopoulos <nvrsr@hotmail.com>
2021-02-05 09:16:32 +0000

    1) Query Sort 2) Pagination: select page and set a limit to findings 3) Courses schema added 4) Seeder module to update the database through the command line (delete and insert the data)

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -14,13 +14,15 @@ exports.getBootcamps = asyncHandler(async (req,res,next)=>{
     const reqQuery = {...req.query}
 
     // Fields to exclude 
-    const removeFields = ['select']
+    const removeFields = ['select','sort','page','limit']
 
     // Loop over removeFields and delete them from reqQuery
     removeFields.forEach(param => delete reqQuery[param])
 
     // create query string
     let queryStr = JSON.stringify(reqQuery)
+
+    //console.log('queryStr = ',queryStr)
 
     // Create operators ($gt, $gte, etc)
     queryStr = queryStr.replace(/\b(gt|gte|lt|lte|in)\b/g,match => `$${match}`)
@@ -34,14 +36,48 @@ exports.getBootcamps = asyncHandler(async (req,res,next)=>{
         query = query.select(fields)
     }
 
+    // sort fields
+    if(req.query.sort){
+        const sortBy = req.query.sort.split(',').join(' ')
+        query = query.sort(sortBy)
+    } else {
+        query = query.sort('-createdAt')
+    }
+
+    // Pagination
+    const page = parseInt(req.query.page) || 1
+    const limit = parseInt(req.query.limit) || 25
+    const startIndex = (page - 1) * limit
+    const endIndex = page * limit
+    const total = await Bootcamp.countDocuments()
+
+    query = query.skip(startIndex).limit(limit)
 
     // Executing query
     const bootcamps = await query
+
+    //Pagination result
+    const pagination = {}
+
+    if(endIndex < total){
+        pagination.next = {
+            page: page + 1,
+            limit
+        }
+    }
+
+    if(startIndex > 0){
+        pagination.prev = {
+            page: page - 1,
+            limit
+        }
+    }
 
 
     res.status(200).json({ 
         success:true,
         count:bootcamps.length,
+        pagination,
         data:bootcamps })
 })
 
